9941c06bf4f33f11aefca0b88803e714ade7f9f4
Subhash Singh <ss5138275@gmail.com>
2025-09-20 17:36:53 +0545

    login page ui and logout functionality

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import {useAuthStore} from "../store/useAuthStore"
 function ChatPage() {
+  const {logout} = useAuthStore();
   return (
-    <div>
+    <div className="z-0">
       chatpage
+      <button onClick={logout}> logout</button>
     </div>
   )
 }
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,7 +4,8 @@ import toast from "react-hot-toast";
 export const useAuthStore = create((set) => ({
   authUser: null,
   isCheckingAuth: true,
-  isSigningUp:false,
+  isSigningUp: false,
+  isLoggingIn:false,
   checkAuth: async () => {
     try {
       const res = await axiosInstance.get("/auth/check");
@@ -12,31 +13,58 @@ export const useAuthStore = create((set) => ({
     } catch (error) {
       console.log("Error in authCheck:", error);
       set({ authUser: null });
-    } finally{
-        set({isCheckingAuth:false})
+    } finally {
+      set({ isCheckingAuth: false });
     }
   },
-  signup:async(data)=>{
-    set({isSigningUp:true })
+  signup: async (data) => {
+    set({ isSigningUp: true });
+    try {
+      const res = await axiosInstance.post("/auth/signup", data);
+      set({ authUser: res.data });
+      console.log(res);
+      // toastify
+      toast.success("Account created successfully!");
+    } catch (error) {
+      if (error.response) {
+        // Server responded with an error status
+        toast.error(error.response.data.message);
+      } else {
+        // Network or unexpected error
+        toast.error(error.message || "Something went wrong");
+      }
+    } finally {
+      set({ isSigningUp: false });
+    }
+  },
+  login: async (data) => {
+    set({ isLoggingIn: true });
+    try {
+      const res = await axiosInstance.post("/auth/login", data);
+      set({ authUser: res.data });
+      console.log(res);
+      // toastify
+      toast.success("Loggedin  successfully!");
+    } catch (error) {
+      if (error.response) {
+        // Server responded with an error status
+        toast.error(error.response.data.message);
+      } else {
+        // Network or unexpected error
+        toast.error(error.message || "Something went wrong");
+      }
+    } finally {
+      set({ isLoggingIn: false });
+    }
+  },
+  logout:async()=>{
     try{
-        const res = await axiosInstance.post("/auth/signup",data);
-        set({authUser:res.data});
-console.log(res)
-        // toastify
-        toast.success("Account created successfully!")
+        await axiosInstance.post("/auth/logout");
+        set({authUser:null});
+        toast.success("Logged out successfully")
     }catch(error){
-            if (error.response) {
-              // Server responded with an error status
-              toast.error(error.response.data.message);
-            } else {
-              // Network or unexpected error
-              toast.error(error.message || "Something went wrong");
-            }
-
-
-    }finally{
-          set({isSigningUp:false })
-
+toast.error("Error logged out");
+console.log("Logout error:",error)
     }
   }
 }));
